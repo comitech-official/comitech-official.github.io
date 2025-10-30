39ca8662ddd40decaf7815bfe5d6a8674b76a05a
Charlotteace <149445926+CharlotteAce@users.noreply.github.com>
2025-10-30 19:34:59 +0900

    v2.1.1 記事のmarkdown化

---
+++ docs/assets/js/link_format.js
@@ -0,0 +1,21 @@
+// /から始まるリンクを絶対パスに、http(https)から始まるリンクを別タブで開くように設定するためのファイルです。
+// 本当はデプロイ時に操作出来たらよいのですが、github page そのままでは難しいためこのような実装としています。
+document.addEventListener("DOMContentLoaded", () => {
+    // Jekyllのsite.urlをJS変数に埋め込む
+    const siteUrl = "{{ site.url }}".replace(/\/$/, ""); // 末尾の / を削除
+
+    document.querySelectorAll("a[href]").forEach(a => {
+        const href = a.getAttribute("href");
+
+        // 1. ルート(/)から始まる相対リンクなら、site.urlを前につける
+        if (href.startsWith("/") && !href.startsWith("//")) {
+            a.href = siteUrl + href;
+        }
+
+        // 2. http または https で始まり、同一ドメインでない場合 → 外部リンク扱い
+        else if (/^https?:\/\//i.test(href) && !href.includes(location.hostname)) {
+            a.setAttribute("target", "_blank");
+            a.setAttribute("rel", "noopener noreferrer");
+        }
+    });
+});
---
+++ docs/contact.md
@@ -0,0 +1,17 @@
+---
+layout: default
+title: お問い合わせ
+description: 工大祭即売会準備会「こみてっく！」のお問い合わせページ。
+---         
+### 連絡先
+- メール：comitech.official\[​a​t​\]gmail.com（\[​a​t​\]をアットマークに置き換えてください）
+- X（旧Twitter）：[@Comitech_offic](https://x.com/Comitech_offic)
+
+### メンバー
+- 代表：じすぷろしうむ（修士２年）
+- 副代表：しのはら恵（修士１年）
+
+　企画メンバー・運営スタッフは現在絶賛募集中です！！（募集要項は[こちら](/staff)）
+
+### 関連リンク
+　近日公開予定です。
